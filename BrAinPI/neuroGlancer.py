61d61fc1d26efd4fad8fb7fd53f2f40071f31059
collin2046 <collin9527@gmail.com>
2025-08-28 15:29:09 -0400

    consideration of shader for RGBA, small bug fix

--- BrAinPI/neuroGlancer.py
+++ BrAinPI/neuroGlancer.py
@@ -789,9 +789,9 @@ def setup_neuroglancer(app, config):
                 # this is only for tif RGB files
                 # logger.info(f"img shape before: {img.shape}")
                 # logger.info(f"img ndim before: {img.ndim}")
-            if img.ndim == 6:
-                # drop first two axes, keep RGB, move channels first
-                img = np.moveaxis(img[0, 0, ..., :3], -1, 0)
+                if img.ndim == 6:
+                    # drop first two axes, keep RGB, move channels first
+                    img = np.moveaxis(img[0, 0, ..., :3], -1, 0)
                 # logger.info(f"img ndim: {img.ndim}")
                 while img.ndim > 4:
                     img = np.squeeze(img, axis=0)
